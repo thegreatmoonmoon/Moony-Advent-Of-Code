e114dbfd9299b434e91017a0bb3e8b8b55d6e7d9
Tomasz Drgas <tomasz.x.drgas@gsk.com>
2019-12-03 20:55:20 +0100

    removed list conversion redundancy

--- Day1/main.py
+++ Day1/main.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     #load input variables from file and convert to int
     inputlist = list(map(int, funcs.load_input("calc_input.csv")))
     
-    #map input variables to fuel calculator and then to totalizer function and get the result
-    result = funcs.fuel_totalizer(*list(map(funcs.fuel_calculator, inputlist)))
+    #map input variables to fuel calculator, pass it to totalizer function and get the result
+    result = funcs.fuel_totalizer(*map(funcs.fuel_calculator, inputlist))
 
-    #map input variables to fuel calculator 2 and then to totalizer function and get the result
-    result2 = funcs.fuel_totalizer(*list(map(funcs.fuel_calculator2, inputlist)))
+    #map input variables to fuel calculator recursive, pass it to totalizer function and get the result
+    result2 = funcs.fuel_totalizer(*map(funcs.fuel_calculator_recursive, inputlist))
 
     #print the result
     print(result)
